4db16c1c654ac48dd9d0cb42f572dfb85ceb4eee
Lloyd Rochester <lloyd.rochester@gmail.com>
2016-06-08 21:28:49 -0500

    Initial Commit

---
+++ ricochet/ricochet.h
@@ -0,0 +1,19 @@
+//
+//  ricochet.h
+//  ricochet
+//
+//  Created by Lloyd Rochester on 6/8/16.
+//  Copyright © 2016 Lloyd Rochester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ricochet.
+FOUNDATION_EXPORT double ricochetVersionNumber;
+
+//! Project version string for ricochet.
+FOUNDATION_EXPORT const unsigned char ricochetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ricochet/PublicHeader.h>
+
+
